7e9cf8033696b73da5535766bb27f13bc7be754a
Juan Diego Gramajo <juan.gramajo@xoom.com>
2016-02-12 10:10:14 -0600

    Initial commit.

---
+++ XoomPayPal.rb
@@ -0,0 +1,11 @@
+if ARGV.nil? || ARGV.size == 0
+  puts "Usage: ruby XoomPayPal.rb <int number number>"
+  exit
+end
+
+ARGV[0].to_i.times { | i |
+  s = ""
+  s += "Xoom" if (i + 1) % 3 == 0
+  s += "PayPal" if (i + 1) % 5 == 0
+  puts s == "" ? i + 1 : s
+}
